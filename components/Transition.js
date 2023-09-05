4ba66ac6b2422ebb10beb55f5eb53e1d331bf064
jomodeDEV <jonathanmonagasdeniz@gmail.com>
2023-09-05 14:45:13 +0200

    Transition done

--- components/Transition.js
+++ components/Transition.js
@@ -1,6 +1,52 @@
+import { motion } from "framer-motion";
+
+const TransitionVariants = {
+  initial: {
+    x: "100%",
+    width: "100%",
+  },
+  animate: {
+    x: "0%",
+    width: "0%",
+  },
+  exit: {
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
+
 const Transition = () => {
   return (
     <>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-slate-800"
+        variants={TransitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ duration: 0.6, ease: "easeInOut" }}
+      >
+      </motion.div>
+
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-slate-700"
+        variants={TransitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.1, duration: 0.6, ease: "easeInOut" }}
+      >
+      </motion.div>
+
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-slate-600"
+        variants={TransitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      >
+      </motion.div>
       <div>Transition</div>
     </>
   );
