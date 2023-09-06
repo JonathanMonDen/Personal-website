f5ea344f37c2245cc287b159069969668c165127
jomodeDEV <jonathanmonagasdeniz@gmail.com>
2023-09-06 16:20:35 +0200

    services done

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75 gap-y-10 w-[350px] xl:w-[560px]">
+      <Image
+        src={"/bulb.png"}
+        width={350}
+        height={350}
+        alt="bulb"
+        className="w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Bulb;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,43 @@
+import {} from "react-icons/rx";
+
+export const serviceData = [];
+
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2 className="text-3xl lg:text-5xl xl:mt-8" variants={fadeIn('left',0.6)}
+        initial="initial"
+        animate="animate"
+        exit="hidden">
+              My services <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p className="mb-4 max-w-[400px] mx-auto lg:mx-0" variants={fadeIn('left',0.6)}
+        initial="initial"
+        animate="animate"
+        exit="hidden">Lorem ipsum</motion.p>
+          </div>
+          <motion.div className="w-full xl:max-w-[65%]" variants={fadeIn('up',0.6)}
+        initial="initial"
+        animate="animate"
+        exit="hidden">
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
 };
 
 export default Services;
